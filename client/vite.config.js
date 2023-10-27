13ed2a932afbcae3da89115570c99b3b3aee9650
Shantanu Sutar <shantanusutar17@gmail.com>
2023-10-28 01:42:18 +0530

    axios error

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+
 export default defineConfig({
   server: {
     proxy: {
       "/api": "https://blog-app-api-pi.vercel.app/",
     },
   },
-  plugins: [react()],
+  plugins: [viteCommonjs(), react()],
 });
