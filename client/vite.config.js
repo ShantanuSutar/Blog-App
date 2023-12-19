b9869b9617c3f49844852e25501bd5b138584617
Shantanu Sutar <shantanusutar17@gmail.com>
2023-12-19 21:43:36 +0530

    deployment error

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [viteCommonjs(), react(), resolve()],
   build: {
     rollupOptions: {
-      external: ["../../api.context.authContext.jsx"],
+      external: ["axios"],
     },
   },
 });
