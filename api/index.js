ba5a050d1ed09535299badba8fd9a1f0a3e1a2f8
Shantanu Sutar <shantanusutar17@gmail.com>
2026-03-17 22:25:27 +0530

    bug: fixing prod cors error

--- api/index.js
+++ api/index.js
@@ -20,8 +20,21 @@ app.use(cookieParser());
 app.use(express.json()); // to send json data to the server
 
 // Use cors middleware with explicit configuration
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['http://localhost:5173', 'http://localhost:5174'];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
